d83a39599ac771c2f39904d67e961b016726785f
hovnatanghukasyan <hovnatan.ghukasyan@picsart.com>
2021-03-13 17:55:28 +0400

    first_version

---
+++ src/Exercise1.java
@@ -0,0 +1,25 @@
+public class Exercise1 {
+    public static void main(String[] args) {
+        String a = "    -    ";
+        String b = "   . .   ";
+        String c = "  | | |  ";
+        String d = " - - - - ";
+        String name = "-  Hovo  -";
+        String e = " - - - - ";
+        String f = "  | | |  ";
+        String g = "   . .   ";
+        String h = "    -    ";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(name);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+
+    }
+}
