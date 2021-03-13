c70fd6d1c0d68b6f8c1854276e25f335685142d2
hovnatanghukasyan <hovnatan.ghukasyan@picsart.com>
2021-03-13 23:01:24 +0400

    better version

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -5,20 +5,16 @@ public class Exercise1 {
         String c = "  | | |  ";
         String d = " - - - - ";
         String name = "-  Hovo  -";
-        String e = " - - - - ";
-        String f = "  | | |  ";
-        String g = "   . .   ";
-        String h = "    -    ";
 
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
         System.out.println(name);
-        System.out.println(e);
-        System.out.println(f);
-        System.out.println(g);
-        System.out.println(h);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(a);
 
 
     }
---
+++ src/Exercise2.java
@@ -0,0 +1,22 @@
+public class Exercise2 {
+    public static void main(String[] args) {
+        char a = '-';
+        char b = '|';
+        char c = '.';
+        char d = 'H';
+        char e = 'o';
+        char f = 'v';
+        char g = ' ';
+
+        System.out.println(g+""+g+""+g+""+g+""+a+""+g+""+g+""+g+""+g);
+        System.out.println(g+""+g+""+g+""+c+""+g+""+c+""+g+""+g+""+g);
+        System.out.println(g+""+g+""+b+""+g+""+b+""+g+""+b+""+g+""+g);
+        System.out.println(g+""+a+""+g+""+a+""+g+""+a+""+g+""+a+""+g);
+        System.out.println(a+""+g+""+d+""+g+""+e+""+g+""+f+""+g+""+e+""+g+""+a);
+        System.out.println(g+""+a+""+g+""+a+""+g+""+a+""+g+""+a+""+g);
+        System.out.println(g+""+g+""+b+""+g+""+b+""+g+""+b+""+g+""+g);
+        System.out.println(g+""+g+""+g+""+c+""+g+""+c+""+g+""+g+""+g);
+        System.out.print(g+""+g+""+g+""+g+""+a+""+g+""+g+""+g+""+g);
+
+    }
+}
